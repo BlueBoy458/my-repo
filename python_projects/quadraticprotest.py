65e99f4aaf7e5461a02ed880f218c763b05655b3
BlueSky <dragonvn7905@gmail.com>
2026-02-07 07:14:03 +0700

    Added some lines of code

--- python_projects/quadraticprotest.py
+++ python_projects/quadraticprotest.py
@@ -1,5 +1,8 @@
-from quadratic_pro import solve
+from quadratic_pro import Equation, solve
 import sys
 
-print(solve("8x**3+1"))
+n = "8x**4 + 1 + 3x**2"
+
+print(Equation(n).parse)
+print(solve(n))
 print(sys.version)
