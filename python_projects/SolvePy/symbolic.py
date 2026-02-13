0cf3ce092f6dc8f58ac63bc413d1a9e8c5c03da8
BlueSky <dragonvn7905@gmail.com>
2026-02-13 14:58:01 +0700

    Recreating

--- python_projects/SolvePy/symbolic.py
+++ python_projects/SolvePy/symbolic.py
@@ -1,49 +1,43 @@
-from quadratic_pro import Equation
+from sqrtpy import PrettyFraction, PrettySqrt
+from re import compile 
+from typing import List, Literal, Self, Union
+from collections.abc import Iterable
+from functools import cached_property
 
-class Symbolic(Equation):
-    #_Equation__allow_symbols = True
+number = r"[+-]?(?:\d+\.\d*|\d+)"
+exponent = r"(?:\*\*\d+)"
+variables = compile(r"[a-zA-Z]")
+remove_exp1 = compile(r"(\*\*0*1(?!\d))|\s")
+whitespaces = compile(r"\s")
+symbols = r"\*?[a-zA-Z]+"
+no_num_exp = rf"{number}|[+-]?(?={symbols}{exponent})"
+sym_pattern = compile(rf"(?:{number}|[+-])?{symbols}{exponent}?")
+no_num_sym = compile(rf"(?={number}|[+-])?{symbols}{exponent}?")
+
+class Polynomial:
     def __init__(self, equation):
-        super().__init__(equation)
-        Equation._unrestrict_symbols()
-        #print(self.find())
-        
-        #super().__init__("x")
-        ...
-        #print(Symbolic.sym_pattern.findall(self.equation))
-    @property
+        self.equation = whitespaces.sub("", equation)
+        #print(sym_pattern.findall(self.equation))
+
+    @cached_property
     def symbol(self):
-       return Symbolic.pattern.findall(self.equation)
-      
-      
-    def find_vars(self):
-        def helper(x):
-            try:
-                return ord(Equation(x).symbol)
-            except TypeError:
-                return 130
-        all_terms = Symbolic.sym_pattern.findall(self.equation)
-        constant = self.equation
-        for x in all_terms:
-            constant = constant.replace(x, "")
-            
-        print(all_terms)
-        #sorted_terms = sorted(all_terms, key=helper)
-        
-        return sorted(all_terms + [constant], key=helper)
-        ...
-        
+        n = variables.findall(self.equation)
+        return tuple(n)
+    
+    
+    @cached_property
+    def values(self):
+        vars = [(no_num_sym.search(x).group(0), compile(number).search(x).group(0)) for x in sym_pattern.findall(self.equation)]
+        return vars
+    
+    
     def __str__(self):
-        exprs = [str(x) for x in self.find_vars()]
-        n = ""
-        for i, x in enumerate(exprs):
-            if i == 0:
-                n += x
-                continue
-            if not any(x.startswith(ops) for ops in "+-"):
-                x = "+" + x
-            n += x.replace("+", " + ").replace("-", " - ")
-        return n
-n = Symbolic("xyz**2 - 8y + 8 + 3 + 7x")
-print(n)
-print(n.find_vars())
-#print(n.equation)
+        return self.equation 
+    
+    def is_common(self, other):
+        return True 
+    
+
+expr = Polynomial("x**2 + 8x + 7x - 2y - 2y= 3")
+print(expr) 
+print(expr.values)
